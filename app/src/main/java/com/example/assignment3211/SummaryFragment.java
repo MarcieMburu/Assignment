a64815b6211311cfb50a3ab57cfc7aff3d1b9631
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-03-17 03:06:12 +0300

    Get and render student course details to UI

--- app/src/main/java/com/example/assignment3211/SummaryFragment.java
+++ app/src/main/java/com/example/assignment3211/SummaryFragment.java
@@ -13,7 +13,12 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.assignment3211.databinding.FragmentSummaryBinding;
+import com.example.assignment3211.models.CourseDetails;
 import com.example.assignment3211.models.Student;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -21,57 +26,65 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class SummaryFragment extends Fragment {
     private FragmentSummaryBinding binding;
-    RecyclerView recyclerView;
-    StudentAd
     FirebaseFirestore db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSummaryBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-
-
         return view;
     }
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         db = FirebaseFirestore.getInstance();
+        DocumentReference studentDocRef =  db.collection("Students").document("1");
+        DocumentReference courseDetailsDocRef = studentDocRef.collection("CourseDetails").document("1");
 
-        // get all students saved in the database
-        db.collection("users").get().addOnCompleteListener(task -> {
+        // get Student data saved in the database
+       studentDocRef.get().addOnCompleteListener(task -> {
             if (!task.isSuccessful()){
                 Toast.makeText(getActivity(), "Error getting student data: "+task.getException().getMessage(), Toast.LENGTH_LONG).show();
                 return;
             }
 
             // data fetched successfully from FireStore
-            Toast.makeText(getActivity(), "Data fetched successfully", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Student data fetched successfully", Toast.LENGTH_SHORT).show();
 
-            QuerySnapshot querySnapshot = task.getResult();
-            for (DocumentSnapshot snapshot : querySnapshot.getDocuments()){
-                Student user = snapshot.toObject(Student.class);
-                // render user interface with user data
-                renderUI(user);
-            }
+            DocumentSnapshot snapshot = task.getResult();
+            Student user = snapshot.toObject(Student.class);
+            // render user interface with user data
+            renderUI(user);
         });
+        // get Student course details
+        courseDetailsDocRef.get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()){
+                Toast.makeText(getActivity(), "Error getting Course Details: "+task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                return;
+            }
 
+            Toast.makeText(getActivity(), "Course Details data fetched successfully", Toast.LENGTH_SHORT).show();
+            DocumentSnapshot snapshot = task.getResult();
+            CourseDetails courseDetails = snapshot.toObject(CourseDetails.class);
+            renderUI(courseDetails);
+        });
     }
 
 
-    // helper method to render user data to the UI
-    private void renderUI(Student user){
-        binding.tvName.setText("Full Names: "+user.getFirstName()+" "+user.getMiddleName()+" "+user.getLastName());
-        binding.tvRegno.setText("Registration Number: "+user.getRegNo());
-        binding.tvIdNo.setText("ID Number: "+user.getIdNo());
-        binding.tvDepartment.setText("Department: "+user.getDepartment());
-        binding.tvCourse.setText("Course: "+user.getCourse());
-        // TODO render student course details
+    // helper method to render Student info
+    private void renderUI(Student student){
+        binding.tvName.setText("Full Names: "+student.getFirstName()+" "+student.getMiddleName()+" "+student.getLastName());
+        binding.tvRegno.setText("Registration Number: "+student.getRegNo());
+        binding.tvIdNo.setText("ID Number: "+student.getIdNo());
+        binding.tvDepartment.setText("Department: "+student.getDepartment());
+        binding.tvCourse.setText("Course: "+student.getCourse());
+    }
 
-
-
-
+    // helper method to render Student Course Details
+    private void renderUI(CourseDetails courseDetails){
+        binding.tvCourse.setText("Course: "+courseDetails.getCourse());
+        binding.tvYear.setText("Year: "+courseDetails.getYear());
+        binding.tvSemester.setText("Semester: "+courseDetails.getSemester());
     }
 }
